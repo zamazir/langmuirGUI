1434b15b310fc060a8bc6db6a27e533f1cd4005a
Amazigh Zerzour <amazigh.zerzour@gmail.com>
2017-08-18 14:40:06 +0200

    Add try statements to shotOverview.py

--- shotOverview.py
+++ shotOverview.py
@@ -16,41 +16,63 @@ fig, axes = subplots(3,1, sharex=True)
 
 # Heating power
 ax = axes[0]
-shot = dd.shotfile('TOT', shotNumber)
-t = shot('P_TOT').time
-y = shot('P_TOT').data / 10**6
-
-ax.plot(t,y)
+try:
+    shot = dd.shotfile('TOT', shotNumber)
+    t = shot('P_TOT').time
+    y = shot('P_TOT').data / 10**6
+except:
+    print "No heating power data"
+else:
+    ax.plot(t,y)
 ax.grid(True)
 ax.set_ylabel('$P_{tot}$ [MW]')
 
 # Tdiv
 ax = axes[1]
-shot = dd.shotfile('DDS', shotNumber)
-t = shot('Tdiv').time
-y = shot('Tdiv').data
-
-ax.plot(t,y)
+try:
+    shot = dd.shotfile('DDS', shotNumber)
+    t = shot('Tdiv').time
+    y = shot('Tdiv').data
+except:
+    print "No divertor temperature data"
+else:
+    ax.plot(t,y)
 ax.grid(True)
 ax.set_ylabel('$T_{div}$ [eV]')
 
 # Puffing & Seeding
 ax = axes[2]
-shot = dd.shotfile('UVS', shotNumber)
-t = shot('D_tot').time
-y = shot('D_tot').data / 10**22
-ax.plot(t,y, label='D fuelling')
+try:
+    shot = dd.shotfile('UVS', shotNumber)
+except:
+    print "No seeding data"
+else:
+    try:
+        t = shot('D_tot').time
+        y = shot('D_tot').data / 10**22
+    except:
+        print "No D fuelling data"
+    else:
+        ax.plot(t,y, label='D fuelling')
 
-t = shot('N_tot').time
-y = shot('N_tot').data / 10**22
-ax.plot(t,y, label='N seeding')
+    try:
+        t = shot('N_tot').time
+        y = shot('N_tot').data / 10**22
+    except:
+        print "No N seeding data"
+    else:
+        ax.plot(t,y, label='N seeding')
 
-t = shot('Ne_tot').time
-y = shot('Ne_tot').data / 10**22
-ax.plot(t,y, label='Ne seeding')
-
+    try:
+        t = shot('Ne_tot').time
+        y = shot('Ne_tot').data / 10**22
+    except:
+        print "No Ne seeding data"
+    else:
+        ax.plot(t,y, label='Ne seeding')
 ax.grid(True)
 ax.set_ylabel('Puffing/Seeding [$10^{22}$/s]')
+
 # Line integrated density
 #ax = axes[2]
 #shot = dd.shotfile('TOT', shotNumber)
